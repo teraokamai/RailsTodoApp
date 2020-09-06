df5c66114ed84d305a62281e6e14a15a3b343668
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-06 18:42:44 +0900

    エラー対応

--- Gemfile
+++ Gemfile
@@ -29,7 +29,6 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 
 gem 'pg'
-gem 'serviceworker-rails'
 
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('serviceworker-companion')
 
 
 // safari起動させないための記述
